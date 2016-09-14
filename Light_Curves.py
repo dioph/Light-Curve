8621d3b315cc6e2a12eb6a8c3dea5f5b676c1b8b
Eduardo Nunes <diofanto.nunes@gmail.com>
2016-09-13 22:32:43 -0300

    New I/O
    Now working with FITS file format and simplified class LightCurve()

--- Light_Curves.py
+++ Light_Curves.py
@@ -1,32 +1,23 @@
-from __future__ import print_function
-import warnings
+#from __future__ import print_function
+import warnings, numpy as np, matplotlib.pyplot as plt
 warnings.filterwarnings("ignore")
-import numpy as np
-import matplotlib.pyplot as plt
 from astropy.visualization import astropy_mpl_style
 from astropy.table import Table
 from os import *
-import pandas as pd
-from pandas import Series, DataFrame, Panel
 from scipy import fftpack
-import tkinter as tk    # GUI
+import lc_io
+#import pandas as pd
+#from pandas import Series, DataFrame, Panel
+#import tkinter as tk    # GUI
 
 class LightCurve(object):
-    def __init__(self, tbl=Table(), N=0, T=0, xcol='', ycol=''):
+    def __init__(self, tbl, N=0, T=0, xcol='time', ycol='sap_flux'):
         self.tbl = tbl
         self.N = N
         self.T = T
         self.xcol = xcol
         self.ycol = ycol
-    def read(self, arq, cwd='C:\\', form='ipac'):
-        self.tbl = Table.read(cwd+arq, format=form) 
-        self.N = len(self.tbl)
-        self.xcol, self.ycol = self.tbl.colnames[1:]
-        self.T = self.tbl[self.xcol][1] - self.tbl[self.xcol][0]
-    def plot(self, xlabel='x', ylabel='y'):
-        plt.plot(self.tbl[self.xcol], self.tbl[self.ycol])
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
+
     def fourier(self):
         xf = np.linspace(0.0, 1/(2.0*self.T), self.N/2)
         yf = fftpack.fft(self.tbl[self.ycol])
@@ -36,12 +27,14 @@ def init():
     plt.style.use(astropy_mpl_style)
     return getcwd()  # current working directory
 
+# I/O
 
 cwd = init()
-lc = LightCurve() 
-lc.read('\plot.tbl', cwd)  # KIC 7447200 light curve
+# KIC 007447200 light curve
+hdulist = lc_io.read('http://archive.stsci.edu/pub/kepler/lightcurves/0074/007447200/kplr007447200-2009166043257_llc.fits')
+lc = LightCurve(tbl=lc_io.extract(hdulist[1]))
 
-# interpolate data to a uniform sampling
+# interpolate data to a uniform sampling [CDA]
 
 '''
 1- points number reduction
@@ -50,12 +43,13 @@ lc.read('\plot.tbl', cwd)  # KIC 7447200 light curve
 '''
 
 # fourier transform [DCDFT]
-
+'''
 xf, yf = lc.fourier()
 plt.plot(xf, 2/lc.N * np.abs(yf[:lc.N/2]))
 
 plt.figure()
-lc.plot(xlabel='Time', ylabel='Flux')
+'''
+lc_io.plot(lc.tbl[lc.xcol], lc.tbl[lc.ycol], xlabel='Time ['+lc.tbl.columns[lc.xcol].unit+']', ylabel='Flux ['+lc.tbl.columns[lc.ycol].unit+']')
 
 plt.show()
 
