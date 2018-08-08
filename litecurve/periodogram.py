1bc69af2f6c97e4143039f0c083e21a281186c25
dioph <diofanto.nunes@gmail.com>
2018-08-08 20:37:55 -0300

    comments

--- litecurve/periodogram.py
+++ litecurve/periodogram.py
@@ -1,11 +1,13 @@
-import numpy as np
-from scipy.stats import linregress
+import warnings
+from time import perf_counter
+
 import matplotlib.pyplot as plt
+import numpy as np
 from astropy.stats import LombScargle as ls
+from scipy.stats import linregress
 
 from .lightcurve import create_from_kic, acf
-from time import perf_counter
-import warnings
+
 warnings.filterwarnings('ignore')
 
 
@@ -22,9 +24,10 @@ def periodogram(kic=None, lc=None, span=None, scale='frequency',
         Timeseries in which to perform the periodogram analysis.
     span : list, optional
         Range of periods to search within.
-        Minimum defaults to 0.5, maximum defaults to 100 or lc.time.max()
+        Minimum defaults to 0.5, maximum defaults to 100 or lc.time.max()/2
     scale : str, optional
         Whether to call LombScargle uniformly on `period` or `frequency`
+        Defaults to `frequency`.
     nperiods : int, optional
         Number of samples in LombScargle, defaults to 1e4+1
     verbose : bool, optional
@@ -36,6 +39,7 @@ def periodogram(kic=None, lc=None, span=None, scale='frequency',
         Best period found with LombScargle
     bp_acf : float
         Best period found with ACF
+    # TODO: return period uncertainties
     """
     t1 = perf_counter()
     if lc is None:
@@ -57,7 +61,7 @@ def periodogram(kic=None, lc=None, span=None, scale='frequency',
     m, b = linregress(lc.time, lc.flux)[:2]
     lc.flux -= m * lc.time + b
 
-    plt.style.use('idl_small')
+    # plt.style.use('idl_small')
 
     if span is None:
         span = [0.5, min(lc.time.max()/2, 100)]
@@ -74,12 +78,14 @@ def periodogram(kic=None, lc=None, span=None, scale='frequency',
     a = lomb.power(freq)
     if verbose:
         print('Calculated periodogram.')
+
     peaks = np.array([i for i in range(1, len(freq)-1)
                       if a[i-1] < a[i] and a[i+1] < a[i]])
     bp_ls = 1./freq[peaks][a[peaks].argsort()[-3:][::-1]]
     fap = lomb.false_alarm_probability(a.max())
     if verbose:
         print('Calculated FAP.')
+
     ml = np.where(lc.time >= span[1])[0][0]
     R = acf(lc.flux, maxlag=ml, plssmooth=True)
     ti, hi = find_peaks(R, lc.time[:ml])
@@ -105,6 +111,10 @@ def periodogram(kic=None, lc=None, span=None, scale='frequency',
     ax[1].set_ylabel('L-S')
     ax[1].set_xlim(span)
     ax[1].text(.825*span[1], .3*a.max(), 'FAP={0:.2f}%'.format(100*fap))
+
+    if fap > 0.05:
+        bp_ls = np.nan
+
     ax[2].plot(lc.time[:ml], R, 'k-')
     ax[2].axhline(0.0, color='gray')
     ax[2].set_ylabel('ACF')
@@ -115,11 +125,11 @@ def periodogram(kic=None, lc=None, span=None, scale='frequency',
     for i,bp in enumerate(bp_acf):
         ax[2].axvline(bp, color='r', ls='--', lw=1)
         ax[2].text(.85*span[1], (.85-.2*i)*abs(size), '{0:.2f} d'.format(bp))
-    if fap > 0.05:
-        bp_ls = np.nan
+
     t2 = perf_counter()
     if verbose:
         print('Done in {0:.2f} s'.format(t2-t1))
+
     return bp_ls, bp_acf, ax
 
 
@@ -129,9 +139,11 @@ def find_peaks(R, lags):
 
     dips = np.array([i for i in range(1, len(lags)-1)
                     if R[i-1] > R[i] and R[i+1] > R[i]])
+
     if lags[dips[0]] > lags[peaks[0]]:
         peaks = peaks[1:]
 
+    '''
     leftdips = np.array([dips[lags[dips] < lags[peak]][-1] for peak in peaks])
 
     if lags[dips[-1]] > lags[peaks[-1]]:
@@ -141,11 +153,14 @@ def find_peaks(R, lags):
         rightdips.append(len(peaks)-1)
         rightdips = np.array(rightdips)
 
-    leftheights = R[leftdips]
-    rightheights = R[rightdips]
-    peakheights = R[peaks]
-    # heights = 0.5 * (np.abs(peakheights-leftheights) +
-    #                   np.abs(peakheights-rightheights))
+     leftheights = R[leftdips]
+     rightheights = R[rightdips]
+     peakheights = R[peaks]
+     heights = 0.5 * (np.abs(peakheights-leftheights) +
+                       np.abs(peakheights-rightheights))
+    '''
+
+    # TODO: Calculate actual peak heights
     heights = R[peaks]
 
     return peaks, heights
